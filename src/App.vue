63fa8a3a8a0018a3726d32b31aed6b24d88e907b
gladja <gladja@yandex.ru>
2023-02-24 23:06:13 +0200

    html code

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
   <div id="app">
-    hello
+
     <input
         @input="onChange"
         v-model="value"
-        type="text"
+        type="number"
     >
     <select v-model="selected">
       <option disabled value="">Выберите один из вариантов</option>
+      <option>USD</option>
       <option>ETH</option>
       <option>BTC</option>
-      <option>USD</option>
+      <option>UAH</option>
     </select>
     <span>Выбрано: {{ selected }}</span>
+
+    <input
+        @input="onChange"
+        type="number"
+        :value="result"
+    >
 
     <select v-model="selected2">
       <option disabled value="">Выберите один из вариантов</option>
       <option>ETH</option>
       <option>BTC</option>
       <option>USD</option>
+      <option>UAH</option>
     </select>
     <span>Выбрано: {{ selected2 }}</span>
 
-    {{ result }}
 
+    <div>
+      {{ result }}
+    </div>
 
 
     <div>ETH {{ ETH }}</div>
@@ -42,8 +52,8 @@ export default {
   data() {
     return {
       value: null,
-      selected: '',
-      selected2: '',
+      selected: 'USD',
+      selected2: 'BTC',
       result: null,
       ETH: null,
       BTC: null,
@@ -61,32 +71,16 @@ export default {
     },
 
     async onChange() {
-      // const API_KEY = 'd2e3ab92213daaaaaad0ca303bf5c6477ed574f7a43bfe4c2541c0de73ff01ee';
-      // const loadTicker = () =>
-      //     fetch(
-      //         `https://min-api.cryptocompare.com/data/price?fsym=${this.selected}&tsyms=${this.selected2}&api_key=${API_KEY}`
-      //     ).then(r => r.json()).then(r => r[this.selected2]);
-      // this.result = await this.getValue()
-      // this.eth = await this.getValue('ETH')
       const [result, ETH, BTC, USD] = await Promise.all([
-          this.getValue(),
-         this.getValue('ETH'),
+        this.getValue(),
+        this.getValue('ETH'),
         this.getValue('BTC'),
         this.getValue('USD'),
-
-
       ])
       this.result = result;
       this.ETH = ETH;
       this.BTC = BTC;
       this.USD = USD;
-
-
-      //(await loadTicker()) * +this.value
-      // console.log(result);
-      // console.log(this.selected2);
-      // console.log(this.value);
-
     },
 
     async getValue(selectedTo = this.selected2) {
@@ -95,7 +89,7 @@ export default {
           fetch(
               `https://min-api.cryptocompare.com/data/price?fsym=${this.selected}&tsyms=${selectedTo}&api_key=${API_KEY}`
           ).then(r => r.json()).then(r => r[selectedTo]);
-      return  (await loadTicker()) * +this.value
+      return (await loadTicker()) * +this.value
     }
 
   }
@@ -105,11 +99,12 @@ export default {
 
 <style>
 #app {
+  max-width: 600px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
 }
 </style>
